25e90a368a5e8f182ecfcf9a96d61a77fdcf2284
kompike <kompanietskostya@gmail.com>
2016-07-14 17:33:14 +0300

    Tests refactored

--- test/test.js
+++ test/test.js
@@ -2,64 +2,93 @@ define = require('node-requirejs-define');
 
 var EventBus = require('../scripts/eventBus');
 
+var test = require('unit.js');
+
 var eventBus = new EventBus();
 
 var firstSubscriberExpectedEvent = "firstData";
 var secondSubscriberExpectedEvent = "secondData";
 
-var firstEventDelivered = false;
-var secondEventDelivered = false;
+var firstEventDelivered = {
+	"delivered":false,
+	"subscriberCounter":0
+};
+
+var secondEventDelivered = {
+	"delivered":false,
+	"subscriberCounter":0
+};
+
+var thirdEventDelivered = {
+	"delivered":false,
+	"subscriberCounter":0
+};
 		
 var firstSubscriber = function(currentEvent) {
 	
-	firstEventDelivered = (currentEvent == firstSubscriberExpectedEvent);
-	
-	console.log(currentEvent + " from f1()");
+	firstEventDelivered.delivered = (currentEvent == firstSubscriberExpectedEvent);
+		
+	firstEventDelivered.subscriberCounter++;
 }
 
 var secondSubscriber = function(currentEvent) {
 	
-	secondEventDelivered = (currentEvent == secondSubscriberExpectedEvent);
-
-	console.log(currentEvent + " from f2()");
+	secondEventDelivered.delivered = (currentEvent == secondSubscriberExpectedEvent);
+	
+	secondEventDelivered.subscriberCounter++;
 }
 
+eventBus.subscribe("first", firstSubscriber);
 eventBus.subscribe("first", firstSubscriber);
 eventBus.subscribe("second", secondSubscriber);
 
 eventBus.post("second", "secondData");
 eventBus.post("first", "firstData");
+eventBus.post("third", "fakeData");
 
 //
-
-var test = require('unit.js');
 
 describe('Testing event bus functionality', function(){
 	
 	it('Event bus successfully created', function(){
-		setTimeout(function(){
+		setTimeout((function(){
 			test
 			.object(eventBus)
 				.isNotEmpty()
 				.hasProperty("subscribe")
 				.hasProperty("post");			
-		}, 2000);
+		})(), 2000);
     });
 	
-	it('First event subscribed', function(){
-		setTimeout(function(){
+	it('First event subscription', function(){
+		
+		setTimeout((function(){
 			test
-				.bool(firstEventDelivered)
-					.isTrue();			
-		}, 2000);
+				.bool(firstEventDelivered.delivered)
+					.isTrue()
+				.number(firstEventDelivered.subscriberCounter)
+					.is(2);
+		})(), 2000);
 	  
 	});
 	
-	it('Second event subscribed', function(){
-		setTimeout(function(){
+	it('Second event subscription', function(){
+		setTimeout((function(){
 			test
-				.bool(secondEventDelivered)
-					.isTrue();			
-		}, 2000);
+				.bool(secondEventDelivered.delivered)
+					.isTrue()
+				.number(secondEventDelivered.subscriberCounter)
+					.is(1);		
+		})(), 2000);
+	});
+	
+	it('Fake event subscription', function(){
+		setTimeout((function(){
+			test
+				.bool(thirdEventDelivered.delivered)
+					.isFalse()
+				.number(thirdEventDelivered.subscriberCounter)
+					.is(0);		
+		})(), 2000);
 	});
 });
